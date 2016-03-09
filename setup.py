7df566b9949d3e973f8e55acd75d81f7006f93bf
sander <s.teunissen@gmail.com>
2016-03-09 13:25:13 +0100

    adding setup file

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from distutils.core import setup
 setup(
     name='powerview_api',
     version='0.2',
-    packages=[''],
-    url='',
-    license='',
     author='sander',
-    author_email='',
-    description='',
     install_requires=['requests']
 )
