46e07b198e555a6c59974192d884e4ed4b335981
sanders76 <s.teunissen@gmail.com>
2016-10-03 17:25:12 +0200

    moving to version 0.3.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='powerviewApi',
-    version='0.3.6',
+    version='0.3.6.1',
     py_modules=['powerview','powerview_async','decode','powerviewbase'],
     author='sander',
 )
