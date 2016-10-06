fac65e5b813d8ad5300364c5d0a2b7dfdaec490d
sanders76 <s.teunissen@gmail.com>
2016-10-06 12:44:53 +0200

    complete_rewrite

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='powerviewApi',
-    version='0.3.7',
-    py_modules=['powerview','powerview_async','decode','powerviewbase'],
+    version='0.3.8',
+    py_modules=['powerview','powerview_async','decode','powerviewbase','shades'],
     author='sander',
 )
